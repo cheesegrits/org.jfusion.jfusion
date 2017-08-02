70b2ed77e9524e7c0f5dba31fe328f60e9f97b85
Hugh Messenger <hugh.messenger@gmail.com>
2017-08-02 16:48:31 -0500

    Change $() to document.id() to avoid jQuery clashes

--- administrator/components/com_jfusion/views/plugindisplay/tmpl/default.js
+++ administrator/components/com_jfusion/views/plugindisplay/tmpl/default.js
@@ -66,7 +66,7 @@ JFusion.deletePlugin = function (jname) {
                 JFusion.onSuccess(JSONobject);
 
                 if (JSONobject.success ===  true && JSONobject.data && JSONobject.data.jname) {
-                    var el = $(JSONobject.data.jname);
+                    var el = document.id(JSONobject.data.jname);
                     el.parentNode.removeChild(el);
                 }
             },
@@ -85,10 +85,10 @@ JFusion.submitForm = function (type) {
         noCache: true,
         format: 'json',
         onRequest: function () {
-            $('spinner'+type).set('html', '<img border="0" alt="loading" src="components/com_jfusion/images/spinner.gif">');
+            document.id('spinner'+type).set('html', '<img border="0" alt="loading" src="components/com_jfusion/images/spinner.gif">');
         },
         onSuccess: function(JSONobject) {
-            $('spinner'+type).set('html', '');
+            document.id('spinner'+type).set('html', '');
             JFusion.onSuccess(JSONobject);
 
             JFusion.updateList(JSONobject);
@@ -96,16 +96,16 @@ JFusion.submitForm = function (type) {
         onError: function (JSONobject) {
             JFusion.OnError(JSONobject);
         }
-    }).post($('install'+type).toQueryString());
+    }).post(document.id('install'+type).toQueryString());
 };
 
 JFusion.downloadPlugin = function () {
-    window.location = $('server_install_url').getSelected().get('value');
+    window.location = document.id('server_install_url').getSelected().get('value');
 };
 
 JFusion.updateList = function (JSONobject) {
     if (JSONobject.success ===  true && JSONobject.data && JSONobject.data.pluginlist) {
-        var list = $('sort_table');
+        var list = document.id('sort_table');
         list.empty();
         list.set('html', JSONobject.data.pluginlist);
         this.initSortables();
@@ -130,7 +130,7 @@ JFusion.initSortables = function () {
             var sortorder;
             //build a string of the order
             sortorder = '';
-            $$('#sort_table tr').each(function (tr) {
+            document.iddocument.id('#sort_table tr').each(function (tr) {
                 sortorder = sortorder +  tr.id  + '|';
             });
 
@@ -154,26 +154,29 @@ JFusion.initSortables = function () {
 };
 
 window.addEvent('domready',function() {
-    $('installSERVER').addEvent('submit', function(e) {
-        e.stop();
-        JFusion.submitForm('SERVER');
-    });
+    var server = document.id('installSERVER');
+    if (server) {
+        server.addEvent('submit', function (e) {
+            e.stop();
+            JFusion.submitForm('SERVER');
+        });
+    }
 
-    $('installDIR').addEvent('submit', function(e) {
+    document.id('installDIR').addEvent('submit', function(e) {
         e.stop();
         JFusion.submitForm('DIR');
     });
 
-    $('installURL').addEvent('submit', function(e) {
+    document.id('installURL').addEvent('submit', function(e) {
         e.stop();
         JFusion.submitForm('URL');
     });
 
-    var installZIP = $('installZIP');
+    var installZIP = document.id('installZIP');
     installZIP.addEvent('submit', function(e) {
         var upload;
         e.stop();
-        $('spinnerZIP').set('html', '<img border="0" alt="loading" src="components/com_jfusion/images/spinner.gif">');
+        document.id('spinnerZIP').set('html', '<img border="0" alt="loading" src="components/com_jfusion/images/spinner.gif">');
         //noinspection JSHint
         upload = new File.Upload({
             url:  JFusion.url,
@@ -185,7 +188,7 @@ window.addEvent('domready',function() {
                 installtype : 'upload'},
             images: ['install_package'],
             onComplete: function (result) {
-                $('spinnerZIP').set('html', '');
+                document.id('spinnerZIP').set('html', '');
                 if (JSON.validate(result)) {
                     var JSONobject = JSON.decode(result);
                     JFusion.onSuccess(JSONobject);
@@ -196,7 +199,7 @@ window.addEvent('domready',function() {
                 }
             },
             onException:  function () {
-                $('installZIP').submit();
+                document.id('installZIP').submit();
             }
         });
         upload.send();
